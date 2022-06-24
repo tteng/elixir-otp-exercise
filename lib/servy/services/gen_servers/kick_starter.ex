f3bdff47d116dfd2a93cbf6047fb589e66188bcd
xianggong <shreadline@gmail.com>
2022-06-24 16:47:48 +0800

    gen server process start and link child process

---
+++ lib/servy/services/gen_servers/kick_starter.ex
@@ -0,0 +1,41 @@
+defmodule Servy.Services.GenServers.KickStarter do
+
+  use GenServer
+
+  @server_name __MODULE__
+
+  def start do
+    IO.puts "Starting the kick starter..."
+    GenServer.start(__MODULE__, :ok, name: @server_name)
+  end
+
+  #callbacks
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    server_pid = start_http_server()
+    {:ok, server_pid}
+  end
+
+  def handle_info({:EXIT, _from, reason}, _state) do
+    IO.puts "http server exit with reason: #{inspect reason}, going to restart it..."
+    server_pid = start_http_server()
+    {:noreply, server_pid}
+  end
+
+  defp start_http_server do
+    IO.puts "Starting the http server..."
+    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    Process.register(server_pid, :http_server)
+    server_pid
+  end
+
+  def handle_call(:get_http_server_pid, _from, state) do
+    {:reply, state, state}
+  end
+
+  #client interface
+  def get_http_server_pid do
+    GenServer.call(@server_name, :get_http_server_pid)
+  end
+
+end
